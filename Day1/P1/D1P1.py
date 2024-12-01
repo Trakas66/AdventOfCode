2b962b81b05cb04d8f6fad47f1008e5ee7e0a134
Jason Trakas <jason.trakas@gmail.com>
2024-12-01 10:24:12 -0500

    First day

---
+++ Day1/P1/D1P1.py
@@ -0,0 +1,17 @@
+file = open(r'../day1.txt')
+input = file.readlines()
+list1, list2 = [], []
+
+for line in input:
+    line = line.split()
+    list1.append(int(line[0]))
+    list2.append(int(line[1]))
+
+list1.sort()
+list2.sort()
+
+total = 0
+for i in range(len(list1)):
+    total += abs(list1[i] - list2[i])
+
+print(total)
---
+++ Day1/P2/D1P2.py
@@ -0,0 +1,18 @@
+file = open(r'../day1.txt')
+input = file.readlines()
+list1, list2 = [], {}
+
+for line in input:
+    line = line.split()
+    list1.append(int(line[0]))
+    if int(line[1]) in list2:
+        list2[int(line[1])] += 1
+    else:
+        list2[int(line[1])] = 1
+
+total = 0
+for i in range(len(list1)):
+    if list1[i] in list2:
+        total += (list1[i] * list2[list1[i]])
+
+print(total)
